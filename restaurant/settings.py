68f635255031dab108cbb9d6d034cbad98db91e5
Henry <hglasspool@googlemail.com>
2024-04-23 09:16:51 +0000

    added secret key to env.py, retrieved in settings

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gfi@l45)k2*yv+e^a=2n=$*mbco-r)b=q0lrkw7@j*na^70bnn'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
