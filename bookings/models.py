1fb1db01561b77e8ea949b7ff9b56d35a574e2aa
Henry <hglasspool@googlemail.com>
2024-05-10 12:39:23 +0000

    created booking model framework

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Bookings(models.Model):
+    date = models.DateField()
+    time = models.TimeField(auto_now=False, auto_now_add=False)
+    guests_number = models.IntegerField(validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ])
+    additional_reqs = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="booking_query"
+    )
+    submitted_on = models.DateTimeField(auto_now_add=True)
