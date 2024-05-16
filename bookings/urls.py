5fa2aed09740515028808ecad896dbe88e84f0ea
Henry <hglasspool@googlemail.com>
2024-05-16 12:17:22 +0000

    corrected form view and model spellings

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.BookingsList.as_view(), name='home'),
+    path('', views.CreateBooking, name='create_booking'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Bookings
+from .forms import BookingForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -13,6 +15,18 @@ class BookingsList(generic.ListView):
             return Bookings.objects.filter(author=self.request.user)
         else:
             return Bookings.objects.none()
+
+def CreateBooking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.author = request.user
+            booking.save()
+            return redirect('bookings')  # Adjust this to your bookings list view URL name
+    else:
+        form = BookingForm()
+    return render(request, 'create_booking.html', {'form': form})
 
 
     
