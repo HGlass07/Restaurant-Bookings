850ca4d535c3376061699c1bfd70a87c0cb06e97
Henry <hglasspool@googlemail.com>
2024-05-16 14:26:23 +0000

    altered booking form to restrict available dates

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Bookings
+from datetime import datetime, timedelta
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Bookings
         fields = ['date', 'time', 'guests_number', 'additional_reqs']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date', 
+            'min': datetime.now() + timedelta(days=7), 
+            'max': (datetime.now() + timedelta(days=365)).date()}),
+
+            'time': forms.TimeInput(attrs={'type': 'time', 
+            'min': '12:00', 'max': '21:00'}),
+        }
