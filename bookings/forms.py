0bd2d6b9fc921c49e0c8dd0b5f2b86d8c9c8026a
Henry <hglasspool@googlemail.com>
2024-05-29 14:20:02 +0000

    adjusted form to conform to pep8

--- bookings/forms.py
+++ bookings/forms.py
@@ -2,29 +2,37 @@ from django import forms
 from .models import Bookings
 from datetime import datetime, timedelta
 
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Bookings
         fields = ['date', 'time', 'guests_number', 'additional_reqs']
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date', 
-                                            'min': (datetime.now() + timedelta(days=7)).strftime('%Y-%m-%d'), 
-                                            'max': (datetime.now() + timedelta(days=365)).strftime('%Y-%m-%d')}),
-            'time': forms.TimeInput(attrs={'type': 'time', 
-                                            'min': '12:00', 'max': '21:00'}),
-            'guests_number': forms.NumberInput(attrs={'min': '1', 'max': '10'}),
+            'date': forms.DateInput(
+                attrs={'type': 'date',
+                       'min': (datetime.now() +
+                               timedelta(days=7)).strftime('%Y-%m-%d'),
+                       'max': (datetime.now() +
+                               timedelta(days=365)).strftime('%Y-%m-%d')}),
+            'time': forms.TimeInput(
+                attrs={'type': 'time', 'min': '12:00', 'max': '21:00'}),
+            'guests_number': forms.NumberInput
+            (attrs={'min': '1', 'max': '10'}),
         }
-    
+
     def clean_date(self):
         date = self.cleaned_data['date']
         if date < datetime.now().date():
             raise forms.ValidationError("You cannot book a date in the past.")
         if date > datetime.now().date() + timedelta(days=365):
-            raise forms.ValidationError("You cannot book more than a year in advance.")
+            raise forms.ValidationError
+            ("You cannot book more than a year in advance.")
         return date
 
     def clean_time(self):
         time = self.cleaned_data['time']
-        if time < datetime.strptime('12:00', '%H:%M').time() or time > datetime.strptime('21:00', '%H:%M').time():
-            raise forms.ValidationError("Your booking must be between 12:00 and 19:00")
+        if (time < datetime.strptime('12:00', '%H:%M').time() or
+           time > datetime.strptime('21:00', '%H:%M').time()):
+            raise forms.ValidationError
+            ("Your booking must be between 12:00 and 19:00")
         return time
