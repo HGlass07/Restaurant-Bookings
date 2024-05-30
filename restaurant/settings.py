f7cde90a45392d5b06c7ec714f7bab9972bb7ff2
Henry <hglasspool@googlemail.com>
2024-05-30 14:07:01 +0000

    uploaded new test menu

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -28,10 +28,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-hglass07-restaurantbook-jlz0ifqae61.ws-eu114.gitpod.io',
+    '8000-hglass07-restaurantbook-jlz0ifqae61.ws-us114.gitpod.io',
     '.herokuapp.com'
 ]
 
